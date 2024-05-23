d82eab67acc25c7064534d7f6bd54cfaf777338b
Robert Rutherford <79166091+neovrtuos@users.noreply.github.com>
2024-05-23 12:40:11 -0700

    Delete ip_blocking.sh

--- ip_blocking.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ports=(1021 8081 8444 8881 33961 2201 63791 3391 50601 1162 1124 1070 8002 2301 14331 5901)
-
-for port in "${ports[@]}"; do
-   iptables -A INPUT -p tcp --dport $port -J DROP
-   iptables -A INPUT -p udp --dport $port -J DROP
-done
